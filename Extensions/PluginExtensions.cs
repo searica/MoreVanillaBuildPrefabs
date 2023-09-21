044d9fd11d279fc0d7876177d22f03a54aa16867
searica <143636061+searica@users.noreply.github.com>
2023-09-20 19:47:05 -0700

    Get values extensions

--- Extensions/PluginExtensions.cs
+++ Extensions/PluginExtensions.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using UnityEngine;
 
 namespace MoreVanillaBuildPrefabs
 {
+
+    public static class TypeExtensions
+    {
+        public static List<T> GetAllPublicConstantValues<T>(this Type type)
+        {
+            return type
+                .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+                .Where(fi => fi.IsLiteral && !fi.IsInitOnly && fi.FieldType == typeof(T))
+                .Select(x => (T)x.GetRawConstantValue())
+                .ToList();
+        }
+
+        public static List<T> GetAllPublicStaticValues<T>(this Type type)
+        {
+            return type
+                .GetFields(BindingFlags.Public | BindingFlags.Static)
+                .Where(fi => fi.FieldType == typeof(T))
+                .Select(x => (T)x.GetValue(null))
+                .ToList();
+        }
+    }
 
     public static class PieceExtensions
     {
