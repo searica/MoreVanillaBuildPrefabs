9b0e02dbefd00bb74debaac0ca50704080ca8c19
searica <143636061+searica@users.noreply.github.com>
2023-10-08 19:03:26 -0700

    Patch armor stand null exceptions

---
+++ Patches/EffectListPatch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+
+namespace MoreVanillaBuildPrefabs.Patches
+{
+    [HarmonyPatch(typeof(EffectList))]
+    internal static class EffectListPatch
+    {
+        /// <summary>
+        ///     Patch to fix null error when using ArmorStand_Male or ArmorStand_Female.
+        /// </summary>
+        /// <param name="__instance"></param>
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(EffectList.Create))]
+        static void CreatePrefix(ref EffectList __instance)
+        {
+            if (!PluginConfig.IsModEnabled.Value)
+            {
+                return;
+            }
+
+            foreach (EffectList.EffectData effectData in __instance.m_effectPrefabs)
+            {
+                if (effectData != null && effectData.m_enabled && !effectData.m_prefab)
+                {
+                    effectData.m_enabled = false;
+                }
+            }
+        }
+    }
+}
