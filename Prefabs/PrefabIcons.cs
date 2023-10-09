d2fb465c27a0d93daab5a6a7412349b91ee410c7
searica <143636061+searica@users.noreply.github.com>
2023-10-09 11:53:33 -0700

    Test delaying icons

--- Prefabs/PrefabIcons.cs
+++ Prefabs/PrefabIcons.cs
@@ -5,17 +5,46 @@ using Jotunn.Managers;
 
 namespace MoreVanillaBuildPrefabs
 {
-    internal class PrefabIcons
+    internal class PrefabIcons : MonoBehaviour
     {
+        private static bool Rendering = false;
+        private static readonly GameObject _parent = new();
+        private static PrefabIcons _instance;
+
+        /// <summary>
+        ///     The singleton instance of this manager.
+        /// </summary>
+        public static PrefabIcons Instance => _instance ??= _parent.AddComponent<PrefabIcons>() as PrefabIcons;
+
+        /// <summary>
+        ///     Hide .ctor to prevent other instances from being created
+        /// </summary>
+        private PrefabIcons() { }
+
+
+        /// <summary>
+        ///     Create and add Icons for list of prefabs with pieces.
+        /// </summary>
+        /// <param name="prefabs"></param>
+        internal void GeneratePrefabIcons(IEnumerable<GameObject> prefabs)
+        {
+            var renderingCoroutine = StartCoroutine(GeneratePrefabIconsCoroutine(prefabs));
+            //var renderWatcher = StartCoroutine(WhileRendering());
+            //StopCoroutine(renderingCoroutine);
+            //StopCoroutine(renderWatcher);
+        }
+
         // Refs:
         //  - CreatureSpawner.m_creaturePrefab
         //  - PickableItem.m_randomItemPrefabs
         //  - PickableItem.RandomItem.m_itemPrefab
-
-        private static IEnumerator GeneratePrefabIcons(IEnumerable<GameObject> prefabs)
+        private IEnumerator GeneratePrefabIconsCoroutine(IEnumerable<GameObject> prefabs)
         {
+            Rendering = true;
             foreach (var prefab in prefabs)
             {
+                if (prefab == null) { Log.LogInfo($"Null prefab found"); }
+
                 yield return null;
                 Sprite result = GenerateObjectIcon(prefab);
                 if (result == null)
@@ -31,12 +60,29 @@ namespace MoreVanillaBuildPrefabs
                         }
                     }
                 }
-                prefab.GetComponent<Piece>().m_icon = result;
+                var piece = prefab.GetComponent<Piece>();
+                if (piece != null)
+                {
+                    piece.m_icon = result;
+                }
             }
+            Rendering = false;
+            Log.LogInfo("Rendering Complete");
         }
 
+        private IEnumerator WhileRendering()
+        {
+            while (Rendering)
+            {
+#if DEBUG
+                Log.LogInfo("Rendering");
+#endif
+                yield return new WaitForSeconds(0.1f);
+            }
 
-        private static Sprite GenerateObjectIcon(GameObject obj)
+        }
+
+        private Sprite GenerateObjectIcon(GameObject obj)
         {
             var request = new RenderManager.RenderRequest(obj)
             {
