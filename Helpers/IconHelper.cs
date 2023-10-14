eba9b5138691097cb23598c4dd1039c3d0d7cb2a
searica <143636061+searica@users.noreply.github.com>
2023-10-14 16:36:33 -0700

    Using coroutine

--- Helpers/IconHelper.cs
+++ Helpers/IconHelper.cs
@@ -1,28 +1,55 @@
-﻿using System.Collections.Generic;
+﻿using System;
+
+​​using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
 using Jotunn.Managers;
+
 using MoreVanillaBuildPrefabs.Logging;
 
 
 namespace MoreVanillaBuildPrefabs.Helpers
 {
-    internal class IconHelper
+    internal class IconHelper : MonoBehaviour
     {
+        private static GameObject _gameObject;
+        private static IconHelper _instance;
+
+        public static IconHelper Instance { get { return _instance; } }
+
         /// <summary>
         ///     Create and add Icons for list of custom pieces.
         /// </summary>
         /// <param name="prefabs"></param>
-        internal static void GeneratePrefabIcons(IEnumerable<Piece> pieces)
+        internal void GeneratePrefabIcons(IEnumerable<GameObject> prefabs)
         {
-            foreach (var piece in pieces)
+
+        }
+
+        private IEnumerator StartRendering(IEnumerable<GameObject> prefabs)
+        {
+            yield return StartCoroutine(StartRendering(prefabs));
+        }
+
+        private IEnumerator RenderCoroutine(IEnumerable<GameObject> gameObjects)
+        {
+            foreach (var gameObject in gameObjects)
             {
+                if (gameObject == null)
+                {
+                    Log.LogWarning($"Null prefab, cannot render icon");
+                    continue;
+                }
+                var piece = gameObject.GetComponent<Piece>();
                 if (piece == null)
                 {
-                    Log.LogInfo($"Null custom piece found");
+                    Log.LogWarning($"Null piece, cannot render icon");
                     continue;
                 }
 
-                Sprite result = GenerateObjectIcon(piece.gameObject);
+                yield return null;
+                Sprite result = GenerateObjectIcon(gameObject);
                 if (result == null)
                 {
                     PickableItem.RandomItem[] randomItemPrefabs = piece.gameObject.GetComponent<PickableItem>()?.m_randomItemPrefabs;
@@ -31,6 +58,7 @@ namespace MoreVanillaBuildPrefabs.Helpers
                         GameObject item = randomItemPrefabs[0].m_itemPrefab?.gameObject;
                         if (item != null)
                         {
+                            yield return null;
                             result = GenerateObjectIcon(item);
                         }
                     }
@@ -41,10 +69,14 @@ namespace MoreVanillaBuildPrefabs.Helpers
 
         private static Sprite GenerateObjectIcon(GameObject obj)
         {
+            var cache = true;
+#if DEBUG
+            cache = false;
+#endif
             var request = new RenderManager.RenderRequest(obj)
             {
                 Rotation = RenderManager.IsometricRotation,
-                UseCache = true
+                UseCache = cache
             };
             return RenderManager.Instance.Render(request);
         }
