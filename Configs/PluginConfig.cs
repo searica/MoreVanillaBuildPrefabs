f3c163ba6343833a9a6033c3fa6fb3871a90811c
searica <143636061+searica@users.noreply.github.com>
2023-10-14 11:15:12 -0700

    Get piece requirments using Jotunn

--- Configs/PluginConfig.cs
+++ Configs/PluginConfig.cs
@@ -5,6 +5,7 @@ using BepInEx;
 using BepInEx.Configuration;
 using UnityEngine;
 using Jotunn.Configs;
+using Jotunn.Managers;
 
 using static MoreVanillaBuildPrefabs.MoreVanillaBuildPrefabs;
 using MoreVanillaBuildPrefabs.Logging;
@@ -257,7 +258,7 @@ namespace MoreVanillaBuildPrefabs.Configs
         /// <returns></returns>
         internal static Piece.Requirement[] CreateRequirementsArray(string data)
         {
-            if (string.IsNullOrEmpty(data.Trim())) return new Piece.Requirement[0];
+            if (string.IsNullOrEmpty(data.Trim())) return Array.Empty<Piece.Requirement>();
 
             // If not empty
             List<Piece.Requirement> requirements = new();
@@ -265,7 +266,8 @@ namespace MoreVanillaBuildPrefabs.Configs
             foreach (var entry in data.Split(';'))
             {
                 string[] values = entry.Split(',');
-                var itm = ObjectDB.instance.GetItemPrefab(values[0].Trim())?.GetComponent<ItemDrop>();
+                var itm = PrefabManager.Cache.GetPrefab<GameObject>(values[0].Trim())?.GetComponent<ItemDrop>();
+                //var itm = ObjectDB.instance.GetItemPrefab(values[0].Trim())?.GetComponent<ItemDrop>();
                 if (itm == null)
                 {
                     Log.LogWarning($"Unable to find requirement ID: {values[0].Trim()}");
