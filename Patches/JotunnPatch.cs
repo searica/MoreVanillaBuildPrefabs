8f7e93e6df166b15adec7744964add26890b33c2
searica <143636061+searica@users.noreply.github.com>
2023-10-14 22:01:03 -0700

    Tidy

---
+++ Patches/JotunnPatch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using Jotunn.Managers;
+
+using MoreVanillaBuildPrefabs.Logging;
+
+namespace MoreVanillaBuildPrefabs.Patches
+{
+    [HarmonyPatch(typeof(SynchronizationManager))]
+    internal class JotunnPatch
+    {
+
+        /// <summary>
+        ///     Disable individual config events when about
+        ///     to recieve config data from the server. This
+        ///     prevents re-intializing 1000+ times if a lot
+        ///     of config entries change at once.
+        ///     
+        ///     A single re-initalization is run after syncing 
+        ///     datat with the server via the
+        ///     SynchronizationManager.OnConfigurationSynchronized event.
+        /// </summary>
+        [HarmonyPrefix]
+        [HarmonyPriority(Priority.Low)]
+        [HarmonyPatch("ConfigRPC_OnClientReceive")]
+        static void ConfigRPC_OnClientReceive_Prefix()
+        {
+            MoreVanillaBuildPrefabs.DisableIndividualConfigEvents = true;
+        }
+    }
+}
