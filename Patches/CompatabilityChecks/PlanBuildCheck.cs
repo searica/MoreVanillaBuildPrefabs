91845346275a82ebef5b655916e021fde7d75488
searica <143636061+searica@users.noreply.github.com>
2023-10-16 12:04:37 -0700

    Log when hooks for other mods occur

---
+++ Patches/CompatabilityChecks/PlanBuildCheck.cs
@@ -0,0 +1,17 @@
+﻿//using HarmonyLib;
+//using MoreVanillaBuildPrefabs.Logging;
+
+//namespace MoreVanillaBuildPrefabs.Patches
+//{
+
+//    [HarmonyPatch(typeof(DungeonDB))]
+//    internal class DungeonDBPatch
+//    {
+//        [HarmonyPostfix]
+//        [HarmonyPatch(nameof(DungeonDB.Start))]
+//        public static void DungeonDBStartPostfix()
+//        {
+//            Log.LogInfo("DungeonDB.Start Postfix (PlanBuild hooks around here)");
+//        }
+//    }
+//}
---
+++ Patches/CompatabilityChecks/PlantEverythingCheck.cs
@@ -0,0 +1,18 @@
+﻿//using HarmonyLib;
+//using MoreVanillaBuildPrefabs.Logging;
+
+//namespace MoreVanillaBuildPrefabs.Patches
+//{
+
+//    [HarmonyPatch(typeof(ZNetScene))]
+//    internal class ZNetScenePatch
+//    {
+//        [HarmonyPostfix]
+//        [HarmonyPatch(nameof(ZNetScene.Awake))]
+//        public static void Postfix(ZNetScene __instance)
+//        {
+//            Log.LogInfo("ZNetSceneAwakePostFix (PlantEverything hooks here)");
+//        }
+//    }
+//}
+
