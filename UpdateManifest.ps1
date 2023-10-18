d93ea5ff5ed4bfbbcd8dfd4a26ac6b245b4066b3
searica <143636061+searica@users.noreply.github.com>
2023-10-18 14:21:19 -0700

    Restructuring

--- UpdateManifest.ps1
+++
@@ -1,9 +0,0 @@
-param ($manifestFile, $versionString)
-
-try {
-    $manifest = Get-Content $manifestFile
-    $manifest = $manifest -replace '"version_number":\s*"([^"]*)"', "`"version_number`": `"$versionString`""
-    Set-Content -Path $manifestFile -Value $manifest
-} catch {
-    Write-Error $_.Exception.Message
-}
