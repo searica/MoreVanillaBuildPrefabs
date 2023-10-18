b156264df68de4171a678ce3d601b1bd56aa1970
searica <143636061+searica@users.noreply.github.com>
2023-10-18 14:22:11 -0700

    always empty inventory of placed pieces

--- Patches/PlayerPatch.cs
+++ Patches/PlayerPatch.cs
@@ -81,20 +81,6 @@ namespace MoreVanillaBuildPrefabs
                         Log.LogInfo($"Emptied inventory for: {gameObject.name}");
                     }
                 }
-
-                //var prefabName = gameObject.name;
-                //if (TreasureChests.ShouldRemoveTreasure(prefabName))
-                //{
-                //    var container = result.GetComponent<Container>();
-                //    if (container != null)
-                //    {
-                //        container.m_inventory.RemoveAll();
-                //        if (PluginConfig.IsVerbosityMedium)
-                //        {
-                //            Log.LogInfo($"Removed treasure for: {prefabName}");
-                //        }
-                //    }
-                //}
             }
             return result;
         }
