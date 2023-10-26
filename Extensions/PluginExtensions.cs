3c09d88c8a6bb9e1cd0ad1f4d770f9cac2c90b9a
searica <143636061+searica@users.noreply.github.com>
2023-10-26 09:15:23 -0700

    GameObject extensions

--- Extensions/PluginExtensions.cs
+++ Extensions/PluginExtensions.cs
@@ -100,55 +100,180 @@ namespace MoreVanillaBuildPrefabs
             return clone;
         }
 
-        public static bool HasComponent<T>(this GameObject go) where T : Component
+        /// <summary>
+        ///     Extension method to check if GameObject has a component.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="gameObject"></param>
+        /// <returns></returns>
+        public static bool HasComponent<T>(this GameObject gameObject) where T : UnityEngine.Component
         {
-            return go.GetComponent<T>() != null;
+            return gameObject.GetComponent<T>() != null;
         }
 
-        public static void DestroyComponent<T>(this GameObject go) where T : UnityEngine.Component
+        /// <summary>
+        ///     Extension method to check if GameObject has a component.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="gameObject"></param>
+        /// <returns></returns>
+        public static bool HasComponent(this GameObject gameObject, string componentName)
         {
-            var components = go.GetComponentsInChildren<T>();
+            return gameObject.GetComponent(componentName) != null;
+        }
+
+        /// <summary>
+        ///     Immediately destroys all instances of a component
+        ///     in the GameObject and all of it's children.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="gameObject"></param>
+        /// <returns></returns>
+        public static void DestroyComponentsInChildren<T>(
+            this GameObject gameObject,
+            bool includeInactive = false
+        ) where T : UnityEngine.Component
+        {
+            var components = gameObject.GetComponentsInChildren<T>(includeInactive);
             foreach (var component in components)
             {
                 UnityEngine.Object.DestroyImmediate(component);
             }
         }
 
-        public static bool HasAnyComponent(this GameObject go, params string[] componentNames)
+        /// <summary>
+        ///     Check if GameObject has any of the specified components.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <param name="components"></param>
+        /// <returns></returns>
+        public static bool HasAnyComponent(
+            this GameObject gameObject,
+            params Type[] components
+        )
         {
-            return componentNames.Any(component => go.GetComponent(component) != null);
+            foreach (var compo in components)
+            {
+                if (gameObject.GetComponent(compo) != null)
+                {
+                    return true;
         }
-
-        public static bool HasAnyComponentInChildren(this GameObject go, params Type[] components)
-        {
-            return components.Any(component => go.GetComponentInChildren(component, true) != null);
-        }
-
-        public static bool HasComponentInChildren<T>(this GameObject go, bool includeInactive = false) where T : Component
-        {
-            return go.GetComponentInChildren<T>(includeInactive) != null;
-        }
-
-        public static bool HasAllComponents(this GameObject go, params string[] componentNames)
-        {
-            return componentNames.All(component => go.GetComponent(component) != null);
+            }
+            return false;
         }
 
         /// <summary>
-        ///     Extension method to return the first component
-        ///     found while searching for components in children
-        ///     that has the specified name.
+        ///     Check if GameObject has any of the specified components.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <param name="components"></param>
+        /// <returns></returns>
+        public static bool HasAnyComponent(
+            this GameObject gameObject,
+            params string[] componentNames
+        )
+        {
+            foreach (var name in componentNames)
+            {
+                if (gameObject.GetComponent(name) != null)
+                {
+                    return true;
+        }
+            }
+            return false;
+        }
+
+        /// <summary>
+        ///     Check if GameObject has all of the specified components.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <param name="components"></param>
+        /// <returns></returns>
+        public static bool HasAllComponents(
+            this GameObject gameObject,
+            params string[] componentNames
+        )
+        {
+            foreach (var name in componentNames)
+            {
+                if (gameObject.GetComponent(name) == null)
+                {
+                    return false;
+        }
+
+        /// <summary>
+        ///     Check if GameObject has all of the specified components.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <param name="components"></param>
+        /// <returns></returns>
+        public static bool HasAllComponents(
+            this GameObject gameObject,
+            params Type[] components
+        )
+        {
+            foreach (var compo in components)
+            {
+                if (gameObject.GetComponent(compo) == null)
+                {
+                    return false;
+        }
+            }
+            return true;
+        }
+
+        /// <summary>
+        ///     Check if GameObject or any of it's children
+        ///     have any of the specified components.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <param name="components"></param>
+        /// <returns></returns>
+        public static bool HasAnyComponentInChildren(
+            this GameObject gameObject,
+            bool includeInactive = false,
+            params Type[] components
+        )
+        {
+            foreach (var compo in components)
+            {
+                if (gameObject.GetComponentInChildren(compo, includeInactive) != null)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        ///     Check if GameObject or any of it's children
+        ///     have the specific component.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <param name="components"></param>
+        /// <returns></returns>
+        public static bool HasComponentInChildren<T>(
+            this GameObject gameObject,
+            bool includeInactive = false
+        ) where T : UnityEngine.Component
+        {
+            return gameObject.GetComponentInChildren<T>(includeInactive) != null;
+        }
+
+        /// <summary>
+        ///     Extension method to get the first component in the GameObject
+        ///     or it's children that has the specified name.
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="gameObject"></param>
         /// <param name="name"></param>
         /// <param name="includeInactive"></param>
         /// <returns></returns>
-        internal static T GetComponentInChildren<T>(
+        internal static T GetComponentInChildrenByName<T>(
             this GameObject gameObject,
             string name,
             bool includeInactive = false
-        ) where T : Component
+        ) where T : UnityEngine.Component
         {
             foreach (
                 var compo in gameObject.GetComponentsInChildren<T>(includeInactive)
@@ -175,7 +300,7 @@ namespace MoreVanillaBuildPrefabs
                     continue;
                 }
 
-                if (NameHelper.RemoveSuffix(mesh.name, "Instance").Trim() == meshName)
+                if (mesh.name.RemoveSuffix("Instance").Trim() == meshName)
                 {
                     return mesh;
                 }
