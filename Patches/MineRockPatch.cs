be604177814d41c4ba507a674d1b8563ec202e55
searica <143636061+searica@users.noreply.github.com>
2023-10-27 11:58:42 -0700

    Fix NRE

---
+++ Patches/MineRockPatch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using MoreVanillaBuildPrefabs.Logging;
+using MoreVanillaBuildPrefabs.Configs;
+
+namespace MoreVanillaBuildPrefabs.Patches
+{
+    [HarmonyPatch(typeof(MineRock))]
+    internal class MineRockPatch
+    {
+        /// <summary>
+        ///     Prevent MineRock from updating visibility until
+        ///     after it acquires a ZNetView (such as by restarting
+        ///     the game or hitting it with something).
+        /// </summary>
+        /// <param name="__instance"></param>
+        /// <returns></returns>
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(MineRock.UpdateVisability))]
+        private static bool UpdateVisabilityPrefix(MineRock __instance)
+        {
+            if (PluginConfig.IsVerbosityMedium)
+            {
+                Log.LogInfo("MineRock.UpdateVisability patch");
+            }
+            return __instance.m_nview != null;
+        }
+    }
+}
