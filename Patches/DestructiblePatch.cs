18d714807cf6341e566e80e4c411836d08adcc50
searica <143636061+searica@users.noreply.github.com>
2023-10-27 12:01:21 -0700

    Fix missing drops

---
+++ Patches/DestructiblePatch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+
+namespace MoreVanillaBuildPrefabs.Patches
+{
+    [HarmonyPatch(typeof(Destructible))]
+    internal class DestructiblePatch
+    {
+        /// <summary>
+        ///     Back to make player-built instances of pieces
+        ///     added by MVBP drop their build resources when destroyed.
+        /// </summary>
+        /// <param name="__instance"></param>
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(Destructible.Destroy))]
+        private static void DestroyPrefix(Destructible __instance)
+        {
+            var prefabName = __instance?.gameObject?.name?.RemoveSuffix("(Clone)");
+            if (MoreVanillaBuildPrefabs.IsPatchedByMod(prefabName))
+            {
+                var piece = __instance?.gameObject?.GetComponent<Piece>();
+                if (piece != null && piece.IsPlacedByPlayer())
+                {
+                    piece.DropResources();
+                }
+            }
+        }
+    }
+}
