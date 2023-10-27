e6ea3e414d95731accd3cf888f155765ec749671
searica <143636061+searica@users.noreply.github.com>
2023-10-26 22:11:40 -0700

    Fix meatpile

---
+++ Patches/PickablePatch.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using MoreVanillaBuildPrefabs.Logging;
+
+namespace MoreVanillaBuildPrefabs.Patches
+{
+    [HarmonyPatch(typeof(Pickable))]
+    internal class PickablePatch
+    {
+        private static readonly DropTable emptyDrops = new();
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(Pickable.RPC_Pick))]
+        private static void RPCPickPrefix(Pickable __instance, out DropTable __state)
+        {
+            var piece = __instance.GetComponent<Piece>();
+            var prefabName = __instance.gameObject.name.RemoveSuffix("(Clone)");
+            if (MoreVanillaBuildPrefabs.IsPatchedByMod(prefabName)
+                && piece != null && piece.IsPlacedByPlayer())
+            {
+                __state = __instance.m_extraDrops;
+                __instance.m_extraDrops = emptyDrops;
+            }
+            else
+            {
+                __state = null;
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(Pickable.RPC_Pick))]
+        private static void RPCPickPostfix(Pickable __instance, DropTable __state)
+        {
+            if (__state != null)
+            {
+                __instance.m_extraDrops = __state;
+            }
+        }
+    }
+}
