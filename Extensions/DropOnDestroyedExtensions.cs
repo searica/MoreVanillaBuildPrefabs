99183d8247b165e30c6f13091ef1f0e9aaac66e2
searica <143636061+searica@users.noreply.github.com>
2023-12-17 12:21:56 -0800

    Get DropOnDestroyed drops

---
+++ Extensions/DropOnDestroyedExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MVBP.Extensions {
+    internal static class DropOnDestroyedExtensions {
+        /// <summary>
+        ///     Extension method to compute the average drops when destroying all hit areas.
+        /// </summary>
+        /// <param name="mineRock5"></param>
+        /// <returns></returns>
+        internal static List<DropTableExtensions.AvgItemDrop> GetAvgDrops(
+            this DropOnDestroyed dropOnDestroyed
+        ) {
+            return dropOnDestroyed.m_dropWhenDestroyed.GetAvgDrops();
+        }
+    }
+}
