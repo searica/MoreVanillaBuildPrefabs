7dcfc259c2e1e55317851c3e072d1d897d51e9f2
searica <143636061+searica@users.noreply.github.com>
2023-12-17 12:20:04 -0800

    Implicit bool optimizations

--- Patches/DropOnDestroyedPatch.cs
+++ Patches/DropOnDestroyedPatch.cs
@@ -1,31 +1,26 @@
 ﻿// Ignore Spelling: MVBP
-
 using HarmonyLib;
 using MVBP.Helpers;
 
-namespace MVBP
-{
+namespace MVBP {
     /// <summary>
     ///     Disables destruction drops for player built pieces.
     ///     Prevents things like player built dvergerprops_crate dropping
     ///     dvergr extractors when extractors were not used to build it.
     /// </summary>
     [HarmonyPatch(typeof(DropOnDestroyed))]
-    internal static class DropOnDestroyedPatch
-    {
+    internal static class DropOnDestroyedPatch {
         [HarmonyPrefix]
         [HarmonyPatch(nameof(DropOnDestroyed.OnDestroyed))]
-        private static bool OnDestroyedPrefix(DropOnDestroyed __instance)
-        {
-            if (InitManager.IsPatchedByMod(__instance))
-            {
-                var piece = __instance.gameObject.GetComponent<Piece>();
-                if (piece != null && piece.IsPlacedByPlayer())
-                {
-                    Log.LogInfo("Disabling on destroyed drops for player-built object", LogLevel.Medium);
-                    return false;
-                }
+        private static bool OnDestroyedPrefix(DropOnDestroyed __instance) {
+            if (__instance &&
+                InitManager.IsPatchedByMod(__instance) &&
+                __instance.TryGetComponent(out Piece piece)
+                && piece.IsPlacedByPlayer()) {
+                Log.LogInfo("Disabling on destroyed drops for player-built object", LogLevel.Medium);
+                return false;
             }
+
             return true;
         }
     }
