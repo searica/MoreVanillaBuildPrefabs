11d735a75e08317261506d74f6d36adc22efce8a
searica <143636061+searica@users.noreply.github.com>
2023-12-17 12:40:48 -0800

    Formatting

--- Patches/ZNetPatch.cs
+++ Patches/ZNetPatch.cs
@@ -4,11 +4,9 @@ using HarmonyLib;
 using MVBP.Helpers;
 using UnityEngine.SceneManagement;
 
-namespace MVBP.Patches
-{
+namespace MVBP.Patches {
     [HarmonyPatch(typeof(ZNet))]
-    internal static class ZNetPatch
-    {
+    internal static class ZNetPatch {
         /// <summary>
         ///     Patch to check if world modifiers for resources are active
         ///     and re-initialize the mod if they are so pickables have the
@@ -16,13 +14,11 @@ namespace MVBP.Patches
         /// </summary>
         [HarmonyPostfix]
         [HarmonyPatch(nameof(ZNet.Start))]
-        public static void ZNetStartPostfix()
-        {
+        public static void ZNetStartPostfix() {
             Log.LogInfo("Checking world modifiers", LogLevel.Medium);
 
             // If loading into game world and prefabs have not been added
-            if (SceneManager.GetActiveScene().name == "main")
-            {
+            if (SceneManager.GetActiveScene().name == "main") {
                 if (Game.m_resourceRate == 1.0f) { return; }
 
                 Log.LogInfo("World modifiers for resource rate are active, re-initializing");
@@ -32,8 +28,7 @@ namespace MVBP.Patches
 
                 InitManager.UpdatePieces();
 
-                if (Log.IsVerbosityMedium)
-                {
+                if (Log.IsVerbosityMedium) {
                     watch.Stop();
                     Log.LogInfo($"Time to re-initialize: {watch.ElapsedMilliseconds} ms");
                 }
