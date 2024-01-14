a6648a64ad68ef84145dc115c41fa9ec836a2d4a
searica <143636061+searica@users.noreply.github.com>
2024-01-14 10:38:48 -0800

    Formatting

--- ModCompat/ModCompat.cs
+++ ModCompat/ModCompat.cs
@@ -6,10 +6,8 @@ using MVBP.Helpers;
 using System;
 using System.Reflection;
 
-namespace MVBP
-{
-    internal static class ModCompat
-    {
+namespace MVBP {
+    internal static class ModCompat {
         public const string ExtraSnapsGUID = "Searica.Valheim.ExtraSnapPointsMadeEasy";
         public const string PlanBuildGUID = "marcopogo.PlanBuild";
         public const string WackysDBGUID = "WackyMole.WackysDatabase";
@@ -24,20 +22,17 @@ namespace MVBP
         private static object PlanDBInstance;
         private static MethodInfo PlanBuildScanTables;
 
-        internal static bool IsWackysDBInstalled()
-        {
+        internal static bool IsWackysDBInstalled() {
             _WackysDBInstalled ??= Chainloader.PluginInfos.ContainsKey(WackysDBGUID);
             return _WackysDBInstalled.Value;
         }
 
-        internal static bool IsPlanBuildInstalled()
-        {
+        internal static bool IsPlanBuildInstalled() {
             _PlanBuildInstalled ??= Chainloader.PluginInfos.ContainsKey(PlanBuildGUID);
             return _PlanBuildInstalled.Value;
         }
 
-        internal static bool IsExtraSnapsInstalled()
-        {
+        internal static bool IsExtraSnapsInstalled() {
             _ExtraSnapsInstalled ??= Chainloader.PluginInfos.ContainsKey(ExtraSnapsGUID);
             return _ExtraSnapsInstalled.Value;
         }
@@ -46,30 +41,24 @@ namespace MVBP
         ///     Triggers a re-initialization of ExtraSnapPointsMadeEasy if it is installed.
         /// </summary>
         /// <returns></returns>
-        internal static bool UpdateExtraSnaps()
-        {
+        internal static bool UpdateExtraSnaps() {
             if (!IsExtraSnapsInstalled()) return false;
 
             var plugin = Chainloader.PluginInfos[ExtraSnapsGUID].Instance;
             if (plugin == null) return false;
 
-            if (ReInitExtraSnapPoints == null)
-            {
-                try
-                {
+            if (ReInitExtraSnapPoints == null) {
+                try {
                     ReInitExtraSnapPoints = ReflectionUtils.GetMethod(plugin.GetType(), "ReInitExtraSnapPoints", Type.EmptyTypes);
                 }
-                catch (Exception e)
-                {
+                catch (Exception e) {
                     Log.LogWarning(e);
                 }
             }
-            try
-            {
+            try {
                 ReInitExtraSnapPoints?.Invoke(plugin, Array.Empty<object>());
             }
-            catch
-            {
+            catch {
                 Log.LogWarning("Could not re-init ExtraSnapPointsMadeEasy");
                 return false;
             }
@@ -80,53 +69,43 @@ namespace MVBP
         ///     Triggers rescanning of piece tables by PlanBuild if it is installed.
         /// </summary>
         /// <returns></returns>
-        internal static bool UpdatePlanBuild()
-        {
+        internal static bool UpdatePlanBuild() {
             // trigger rescanning of piece tables via reflection
-            if (TryGetPlanDBInstance(out object planDBInstance))
-            {
+            if (TryGetPlanDBInstance(out object planDBInstance)) {
                 Log.LogInfo("PlanBuild is installed", LogLevel.Medium);
-                try
-                {
+                try {
                     var method = GetPlanBuildScanTables();
                     Log.LogInfo("Triggering PlanBuild ScanPieceTables", LogLevel.Medium);
                     method?.Invoke(planDBInstance, Array.Empty<object>()); // Invoke if not null
                     return true;
                 }
-                catch
-                {
-                    Log.LogWarning("Failed to trigger PlanBuild ScanPieceTables");
+                catch (Exception e) {
+                    Log.LogWarning($"Failed to update PlanBuild:\n {e}");
                 }
             }
             return false;
         }
 
-        private static MethodInfo GetPlanBuildScanTables()
-        {
-            if (PlanBuildScanTables == null)
-            {
+        private static MethodInfo GetPlanBuildScanTables() {
+            if (PlanBuildScanTables == null) {
                 PlanBuildScanTables = AccessTools.Method("PlanBuild.Plans.PlanDB:ScanPieceTables", Type.EmptyTypes);
             }
             return PlanBuildScanTables;
         }
 
-        private static bool TryGetPlanDBInstance(out object planDBInstance)
-        {
-            if (PlanDBInstance == null && TryGetPlanDBType(out Type planDBType))
-            {
+        private static bool TryGetPlanDBInstance(out object planDBInstance) {
+            if (PlanDBInstance == null && TryGetPlanDBType(out Type planDBType)) {
                 MethodInfo planDBGetter = AccessTools.PropertyGetter(planDBType, "Instance");
                 PlanDBInstance = planDBGetter.Invoke(null, Array.Empty<object>());
                 planDBInstance = PlanDBInstance;
             }
-            else
-            {
+            else {
                 planDBInstance = PlanDBInstance;
             }
             return planDBInstance != null;
         }
 
-        private static bool TryGetPlanDBType(out Type planDBType)
-        {
+        private static bool TryGetPlanDBType(out Type planDBType) {
             planDBType = null;
             if (!IsPlanBuildInstalled()) { return false; }
 
@@ -140,10 +119,8 @@ namespace MVBP
             if (plugin == null) return false;
 
             var assembly = Assembly.GetAssembly(plugin.GetType());
-            foreach (var type in AccessTools.GetTypesFromAssembly(assembly))
-            {
-                if (type.FullName == "PlanBuild.Plans.PlanDB")
-                {
+            foreach (var type in AccessTools.GetTypesFromAssembly(assembly)) {
+                if (type.FullName == "PlanBuild.Plans.PlanDB") {
                     PlanDBType = type;
                     planDBType = type;
                     return true;
@@ -153,8 +130,7 @@ namespace MVBP
             return false;
         }
 
-        internal static bool IsWackyDBClone(string name)
-        {
+        internal static bool IsWackyDBClone(string name) {
             if (!IsWackysDBInstalled()) return false;
 
             var plugin = Chainloader.PluginInfos[WackysDBGUID].Instance;
