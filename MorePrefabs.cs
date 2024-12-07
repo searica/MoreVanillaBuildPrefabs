ca42af604a046ae0b5549c3f283b87ef166d2eb7
searica <143636061+searica@users.noreply.github.com>
2024-12-07 09:16:16 -0800

    Update: build 1.3.1

--- MorePrefabs.cs
+++ MorePrefabs.cs
@@ -26,7 +26,7 @@ namespace MVBP
         public const string PluginName = "MoreVanillaBuildPrefabs";
         internal const string Author = "Searica";
         public const string PluginGUID = $"{Author}.Valheim.{PluginName}";
-        public const string PluginVersion = "1.3.0 ";
+        public const string PluginVersion = "1.3.1 ";
 
         #region Global Settings
 
