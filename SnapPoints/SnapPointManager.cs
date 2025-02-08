f85b4965277394b6d83d2f71fd0016270e84efae
searica <143636061+searica@users.noreply.github.com>
2025-02-08 08:52:16 -0800

    Update snap point names

--- SnapPoints/SnapPointManager.cs
+++ SnapPoints/SnapPointManager.cs
@@ -45,6 +45,63 @@ internal static class SnapPointManager
     //    new Vector3(1, 1, 0),
     //};
 
+    internal static void AddSnapPoints(
+        GameObject gameObject,
+        IEnumerable<Vector3> points
+    )
+    {
+        int i = 1;
+
+        foreach (Vector3 point in points)
+        {
+            Vector3 pos = point;
+            CreateSnapPoint(pos, gameObject, $"{SnapPointNames.SNAPPOINT} {i++}");
+        }
+    }
+
+    internal static void AddSnapPoints(
+        GameObject gameObject,
+        NamedSnapPoint[] namedSnapPoints
+    )
+    {
+        foreach (NamedSnapPoint point in namedSnapPoints)
+        {
+            CreateSnapPoint(point.LocalPosition, gameObject, point.Name);
+        }
+    }
+
+    private static void CreateSnapPoint(Vector3 pos, GameObject prefab, string snapPointName)
+    {
+        GameObject snappoint = new(snapPointName);
+        snappoint.transform.parent = prefab.transform;
+        snappoint.transform.localPosition = pos;
+        snappoint.tag = SnapPointNames.TAG;
+        snappoint.SetActive(false);
+    }
+
+    
+    /// <summary>
+    ///     Set all colliders to the piece layer.
+    /// </summary>
+    /// <param name="gameObject"></param>
+    internal static void FixPieceLayers(GameObject gameObject)
+    {
+        foreach (Collider collider in gameObject.GetComponentsInChildren<Collider>())
+        {
+            collider.gameObject.layer = LayerMask.NameToLayer("piece");
+        }
+    }
+
+    /// <summary>
+    ///     Adds a snap point to the local center of the game object's transform.
+    /// </summary>
+    /// <param name="gameObject"></param>
+    internal static void AddSnapPointToOrigin(GameObject gameObject)
+    {
+        CreateSnapPoint(Vector3.zero, gameObject, ORIGIN);
+    }
+
+
     /// <summary>
     ///     Adds snap points for the game object to the corners of the specified mesh.
     /// </summary>
@@ -61,7 +118,7 @@ internal static class SnapPointManager
             return;
         }
 
-        List<Vector3> pts = new();
+        List<Vector3> pts = [];
         Vector3 extents = boxCollider.size / 2;
         foreach (Vector3 corner in corners)
         {
@@ -95,60 +152,5 @@ internal static class SnapPointManager
             pts.Add(bounds.center + Vector3.Scale(corner, bounds.extents));
         }
         AddSnapPoints(gameObject, pts);
-    }
-
-    /// <summary>
-    ///     Adds a snap point to the local center of the game object's transform.
-    /// </summary>
-    /// <param name="gameObject"></param>
-    internal static void AddSnapPointToOrigin(GameObject gameObject)
-    {
-        CreateSnapPoint(Vector3.zero, gameObject, ORIGIN);
-    }
-
-    internal static void AddSnapPoints(
-        GameObject gameObject,
-        IEnumerable<Vector3> points
-    )
-    {
-        int i = 1;
-
-        foreach (Vector3 point in points)
-        {
-            Vector3 pos = point;
-            CreateSnapPoint(pos, gameObject, $"{SNAPPOINT} {i++}");
-        }
-    }
-
-    internal static void AddSnapPoints(
-        GameObject gameObject,
-        NamedSnapPoint[] namedSnapPoints
-    )
-    {
-        foreach (NamedSnapPoint point in namedSnapPoints)
-        {
-            CreateSnapPoint(point.LocalPosition, gameObject, point.Name);
-        }
-    }
-
-    private static void CreateSnapPoint(Vector3 pos, GameObject prefab, string snapPointName)
-    {
-        GameObject snappoint = new(snapPointName);
-        snappoint.transform.parent = prefab.transform;
-        snappoint.transform.localPosition = pos;
-        snappoint.tag = "snappoint";
-        snappoint.SetActive(false);
-    }
-
-    /// <summary>
-    ///     Set all colliders to the piece layer.
-    /// </summary>
-    /// <param name="gameObject"></param>
-    internal static void FixPieceLayers(GameObject gameObject)
-    {
-        foreach (Collider collider in gameObject.GetComponentsInChildren<Collider>())
-        {
-            collider.gameObject.layer = LayerMask.NameToLayer("piece");
-        }
     }
 }
